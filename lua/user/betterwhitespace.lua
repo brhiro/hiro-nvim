fc09bfba2b320aac9faa7a685568e8c68d00d201
brhiro <49073956+brhiro@users.noreply.github.com>
2023-02-03 15:59:01 -0800

    Ensure correct filetypes in vim-better-whitespace blacklist

---
+++ lua/user/betterwhitespace.lua
@@ -0,0 +1,17 @@
+vim.g.better_whitespace_filetypes_blacklist = {
+	"diff",
+	"git",
+	"gitcommit",
+	"unite",
+	"qf",
+	"help",
+	"markdown",
+	"fugitive",
+	"toggleterm",
+	"startify",
+	"dashboard",
+	"packer",
+	"neogitstatus",
+	"NvimTree",
+	"Trouble",
+}
